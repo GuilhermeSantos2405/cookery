99671127bed11411c08b8c835430b8ebbd5cdaf9
Guilherme <guilherme_108@hotmail.com>
2022-05-25 23:15:52 -0300

    Criando models e admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at',
+                    'updated_at', 'author', 'category', 'is_published']
+    list_display_links = ('id', 'title')
+    list_editable = ('is_published',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+
+    preparation_time = models.IntegerField()
+    servings = models.IntegerField()
+    ingredients = models.TextField()
+    method_preparation = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False, blank=True)
+    image = StdImageField(upload_to='path/to/img',
+                          variations={'variation_image':
+                                      {'width': 370, 'height': 217}})
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = ('Recipe')
+        verbose_name_plural = ('Recipes')
