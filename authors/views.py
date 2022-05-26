b38a190536342e3aff595fb302add1e72438d154
Guilherme <guilherme_108@hotmail.com>
2022-05-26 16:51:04 -0300

    Finalizado o cadastrar, login e logout

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import Registerform
+
+
+class AuthorsCreateView(CreateView):
+    template_name = 'authors/templates/create_authors.html'
+    form_class = Registerform
+    success_url = reverse_lazy('index')
