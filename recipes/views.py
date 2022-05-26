ac200012349cd86475f617551abc66517d4b64f9
Guilherme <guilherme_108@hotmail.com>
2022-05-26 10:35:56 -0300

    Apresentando dados do BD no template

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from .models import Recipe
 
 
-class IndexTemplateView(TemplateView):
+class IndexTemplateView(ListView):
     template_name = 'pages/index.html'
+    context_object_name = 'recipes_list'
+    model = Recipe
+    recipe = Recipe.objects.all()
